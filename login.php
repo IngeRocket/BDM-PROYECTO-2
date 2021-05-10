12d5b852a7b52fa333215188ba4d1beb87860fef
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-05-10 16:15:47 -0500

    Proyecto
    Formato login y registro

--- login.php
+++ login.php
@@ -2,26 +2,45 @@
 <html>
 <head>
 	<title>login</title>
-	<link rel="stylesheet" type="text/css" href="styles/general.css">
+	<link rel="stylesheet" type="text/css" href="styles/navbar.css">
+	<link rel="stylesheet" type="text/css" href="styles/login.css">
 </head>
 <body>
 	<?php include 'php/capaModelo.php'; ?>
 
 	<div class="contenedor">
-		
-		<form method="post">
-		<label>Tipo de cuenta</label>
-		<select name="u-cuenta">
-			<option value="1">Alumno</option>
-			<option value="2">Instructor</option>
-		</select>
-		<label>correo electronico</label>
-		<input type="text" name="u-name" required placeholder="email@example.com">
-		<label>clave de acceso</label>
-		<input type="password" name="u-pass" required placeholder="password">
-		<button type="submit" name="enviar">Enviar</button>
-		</form>
-
+		<header class="header">
+			<div class="menu">
+				<div class="logo"><a href="index.php"><img src="img/navbar/escuela-logo.png"></a></div>
+				<div class="categorias">
+					<div class="categoria">Mas vendidos</div>
+					<div class="categoria">Mas populares</div>
+					<div class="categoria">Mas recientes</div>
+					<div class="categoria">Mejor promediados</div>
+				</div>
+				<div class="categorias">
+					<div class="categoria">Categorias</div>
+				</div>
+			</div>
+		</header>
+		<div class="contenido">
+			<form method="post">
+			<label>Tipo de cuenta</label>
+			<select name="u-cuenta">
+				<option value="1">Alumno</option>
+				<option value="2">Instructor</option>
+			</select>
+			<label>correo electronico</label>
+			<input type="email" name="u-name" required placeholder="email@example.com">
+			<label>clave de acceso</label>
+			<input type="password" name="u-pass" required placeholder="password">
+			<button type="submit" name="enviar">Iniciar Sesion</button>
+			</form>
+		</div>
+		<div class="navegacion">
+			<p>No tienes cuenta?. Oprime el boton de abajo para ir a crear una cuenta nueva</p>
+			<a href="registro.php">Ir a registro</a>
+		</div>
 	</div>
 	<?php 
 		if(isset($_POST['enviar'])){
--- registro.php
+++ registro.php
@@ -3,25 +3,45 @@
 <head>
 	<title>Registro</title>
 </head>
-<link rel="stylesheet" type="text/css" href="styles/general.css">
+<link rel="stylesheet" type="text/css" href="styles/navbar.css">
+<link rel="stylesheet" type="text/css" href="styles/registro.css">
 <body>
 	<?php include 'php/capaModelo.php'; ?>
 	<div class="contenedor">
-		<form method="post">
-			<label>Tipo de cuenta</label>
-			<select name="cuenta">
-				<option value="1">Alumno</option>
-				<option value="2">Instructor</option>
-			</select>
-			<label>Nombre completo</label>
-			<input type="text" name="u-nombre" required placeholder="Nombre Completo">
-			<label>correo electronico</label>
-			<input type="text" name="u-correo" required placeholder="example@correo.com">
-			<label>clave de acceso</label>
-			<input type="password" name="u-pass" required placeholder="password">
-			<button type="submit" name="registro">Registro</button>
-		</form>
-		<a href="login.php">Ir a Inicio de Sesion</a>
+		<header class="header">
+			<div class="menu">
+				<div class="logo"><a href="index.php"><img src="img/navbar/escuela-logo.png"></a></div>
+				<div class="categorias">
+					<div class="categoria">Mas vendidos</div>
+					<div class="categoria">Mas populares</div>
+					<div class="categoria">Mas recientes</div>
+					<div class="categoria">Mejor promediados</div>
+				</div>
+				<div class="categorias">
+					<div class="categoria">Categorias</div>
+				</div>
+			</div>
+		</header>
+
+		<div class="contenido">
+			<form method="post">
+				<label>Tipo de cuenta</label>
+				<select name="cuenta">
+					<option value="1">Alumno</option>
+					<option value="2">Instructor</option>
+				</select>
+				<label>Nombre completo</label>
+				<input type="text" name="u-nombre" required placeholder="Nombre Completo">
+				<label>correo electronico</label>
+				<input type="text" name="u-correo" required placeholder="example@correo.com">
+				<label>clave de acceso</label>
+				<input type="password" name="u-pass" required placeholder="password">
+				<button type="submit" name="registro">Registro</button>
+			</form>
+		</div>
+		<div class="navegacion">
+			<a href="login.php">Ir Inicio de Sesion</a>
+		</div>
 	</div>
 	<?php 
 		if(isset($_POST['registro'])){
