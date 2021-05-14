2d25c78b46801412df14ad026406c8f4547dd096
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-05-13 22:44:33 -0500

    Proyecto
    prototipo de pantallas

---
+++ buscador.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Historial</title>
+	<link rel="stylesheet" type="text/css" href="styles/historial.css">
+</head>
+<body>
+	<div class="contenedor">
+		<div class="pantalla">
+			<div class="p-panel"> panel </div>
+			<div class="p-resultado">
+				<div class="buscador">
+					<div class="caja">
+						<input type="text" name="" placeholder="titulo de curso...">
+					</div>
+					<div class="boton"><button>buscar</button></div>
+				</div>
+				<div class="r-contenido">
+					<div class="tarjeta">
+						<div class="t-imagen"><img src="img/fondo.png"></div>
+						<div class="t-titulo">
+							<div class="titulo-nombre">Nombre del cuso</div>
+							<div class="titulo-categoria">CSS</div>
+						</div>
+						<div class="t-progreso">progreso</div>
+						<div class="t-estado">estado</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ historial.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Historial</title>
+	<link rel="stylesheet" type="text/css" href="styles/historial.css">
+</head>
+<body>
+	<div class="contenedor">
+		<div class="pantalla">
+			<div class="p-panel">
+			 	<div class="panel">
+			 		<label>Mostrar cursos:</label>
+			 		<select>
+			 			<option>Todos</option>
+			 			<option>Inscrito (Mixto)</option>
+			 			<option>Comprados</option>
+			 		</select>
+			 		<button>Aplicar</button>
+			 	</div>
+			</div>
+			<div class="p-resultado">
+				<div class="r-contenido">
+					<div class="cabecera">
+						<div class="t-imagen"><label>Miniatura</label></div>
+						<div class="t-titulo">
+							<div class="titulo-nombre">Nombre del cuso</div>
+							<div class="titulo-categoria"></div>
+						</div>
+						<div class="t-progreso">Progreso</div>
+						<div class="t-estado">Estado</div>
+					</div>
+					<div class="tarjeta">
+						<div class="t-imagen"><img src="img/fondo.png"></div>
+						<div class="t-titulo">
+							<div class="titulo-nombre">Nombre del cuso</div>
+							<div class="titulo-categoria">CSS</div>
+						</div>
+						<div class="t-progreso">progreso</div>
+						<div class="t-estado">estado</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
